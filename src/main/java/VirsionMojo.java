de0be9688ced9400e5ab3819c5e65a87d9455515
lianakanukoeva <lianakanukoeva@yandex.ru>
2021-10-18 15:03:56 +0300

    change class name

--- src/main/java/VirsionMojo.java
+++ src/main/java/VirsionMojo.java
@@ -1,5 +1,5 @@
 @Mojo(name = "version")
-public final class VersionMojo extends AbstractMojo {
+public final class VirsionMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", required = true, readonly = true)
     public MavenProject project;

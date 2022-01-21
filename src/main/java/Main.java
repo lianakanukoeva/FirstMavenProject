5b9732a2fb6202e264016f8ec211a98df0e58c77
lianakanukoeva <lianakanukoeva@yandex.ru>
2022-01-21 16:47:13 +0300

    added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,8 @@
-
+import static jdk.javadoc.internal.tool.Main.execute;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
-//        String sql = "SELECT * FROM test";
-//
-//        try (Connection connection = DriverManager.getConnection(URL)) {
-//            System.out.println(String.format("%s", conok));
-//            Statement statement = connection.createStatement();
-//
-//            statement.execute(sql);
-//
-//            boolean isExecuted = statement.execute(sql);
-//
-//            if (isExecuted) {
-//                System.out.println("Select executed");
-//            }
-//        } catch (SQLException e) {
-//            System.out.println(String.format("%s", conerr));
-//            e.printStackTrace();
-//        }
+        System.out.println(execute());
     }
 }
